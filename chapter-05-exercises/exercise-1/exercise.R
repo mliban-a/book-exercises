a6be1856aa4880fd272457c7c58325eeab5512bd
mliban-a <mliban@uw.edu>
2020-01-17 13:59:53 -0800

    In Class Exercises

--- chapter-05-exercises/exercise-1/exercise.R
+++ chapter-05-exercises/exercise-1/exercise.R
@@ -1,22 +1,27 @@
-# Exercise 1: practice with basic R syntax
+# CH 5. Exercise 1: practice with basic R syntax
 
 # Create a variable `hometown` that stores the city in which you were born
-
+hometown_city <- "Seattle"
 
 # Assign your name to the variable `my_name`
 
+my_name <- "Mursal"
 
 # Assign your height (in inches) to a variable `my_height`
 
+my_height <- 68"'"
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 
+puppies <- 0
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
 
+puppy_price <- 800
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
 
+total_cost <- puppy_price + puppies
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -1,12 +1,16 @@
-# Exercise 3: writing and executing functions
+# CH 6 Exercise 3: writing and executing functions
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
+add_three <- function( greater ) {
+  return <- (greater + 3)
+  
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
-
+ ten <- add_three(7)
+ print(ten)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
